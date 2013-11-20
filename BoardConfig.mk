d803b3522c063ff0127cf6c47036f29199be27f0
Koushik Dutta <koushd@gmail.com>
2013-11-20 04:07:58 -0800

    HACK: Disable secure discard
     * Secure discard is causing wipes to take insane amounts of time.
     * Enable this workaround until the problem is sorted.
    
    Change-Id: If9bbb27fd5476a160e9d7e8b29ab20a851410ff7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,6 +150,9 @@ TARGET_TOUCHBOOST_FREQUENCY:= 1200
 USE_DEVICE_SPECIFIC_QCOM_PROPRIETARY:= true
 USE_DEVICE_SPECIFIC_CAMERA:= true
 
+# Flags
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+
 # Enable Minikin text layout engine (will be the default soon)
 USE_MINIKIN := true
 
