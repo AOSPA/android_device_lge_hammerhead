911b36d01e1c1a87f5fc1843e028a6a6774f1be3
Lars Greiss <kufikugel@googlemail.com>
2014-12-07 15:39:13 +0100

    Hammerhead: Disable touch boost debug spam
    we use our own kernel implementation but still want to keep up
    compatibility to third party kernels which may use stock mpdecision.
    
    Therefore we simply disable debug for it so that we do not have complains
    about missing stock mpdecision.
    
    Change-Id: I06eb0953ea6c5750948ce3e6150470dcf977abb5
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,8 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 maxcpus=2 msm_watchdog_v2.enable=1
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02900000 --tags_offset 0x02700000
 
+TOUCH_BOOST_DEBUG := false
+
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
 # Increase the size if shaders of size greater than 12KB are used.
