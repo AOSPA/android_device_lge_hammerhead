d3cff695b705d400f2ae4a1979ea94f5f3281e8b
BigBrother1984 <carlosavignano@aospa.co>
2014-12-22 00:16:46 +0100

    hammerhead: Initial PA lollipop bringup
    Signed-off-by: BigBrother1984 <carlosavignano@aospa.co>
    Change-Id: I265d887ebf9b502457c4e2f398efe4ebd8e9a62c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,8 +150,6 @@ TARGET_TOUCHBOOST_FREQUENCY:= 1200
 USE_DEVICE_SPECIFIC_QCOM_PROPRIETARY:= true
 USE_DEVICE_SPECIFIC_CAMERA:= true
 
--include vendor/lge/hammerhead/BoardConfigVendor.mk
-
 # Enable Minikin text layout engine (will be the default soon)
 USE_MINIKIN := true
 
