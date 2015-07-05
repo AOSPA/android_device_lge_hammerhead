5b3654c623c3053bea3d49e04056918b4394ee33
Paul Beeler <pbeeler80@gmail.com>
2015-07-05 16:07:20 -0400

    Inline kernel building (1/3)
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

---
+++ AndroidBoard.mk
@@ -0,0 +1,3 @@
+# This is a dummy file for android's way of picking up on AndroidBoard.mk for inline kernel building.  The real magic is in vendor/pa/configs/kernel.mk
+
+include vendor/pa/configs/kernel.mk
