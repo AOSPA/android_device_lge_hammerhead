8afb6d179ec3ecbf6641af53f0660423ea5bd15b
beanstown106 <nbenis106@gmail.com>
2015-10-24 02:52:12 -0400

    lets do the battery some favors and not light it on fire..
    Change-Id: I425dee04eaa2e0a4a8354007d1c8f3faeae6084e

--- device.mk
+++ device.mk
@@ -307,8 +307,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.call_ring.multiple=0
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    wifi.interface=wlan0 \
-    wifi.supplicant_scan_interval=15
+    wifi.interface=wlan0
 
 # Enable AAC 5.1 output
 PRODUCT_PROPERTY_OVERRIDES += \
