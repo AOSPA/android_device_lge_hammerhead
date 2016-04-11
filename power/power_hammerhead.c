d1163a3d67ad8c969bd2c45380d4ad77350b8b06
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-04-11 11:13:10 -0400

    PowerHAL: Remove touchboost
    Stop logcat spam "cannot connect" touch_boost error messages
    
    Change-Id: If09fcd8ee9762c47e92695a948f716f1a20d7378

--- power/power_hammerhead.c
+++ power/power_hammerhead.c
@@ -296,28 +296,6 @@ static void process_video_encode_hint(void *metadata)
     }
 }
 
-
-static void touch_boost()
-{
-    int rc;
-    pid_t client;
-    char data[MAX_LENGTH];
-
-    if (client_sockfd < 0) {
-        ALOGE("%s: boost socket not created", __func__);
-        return;
-    }
-
-    client = getpid();
-
-    snprintf(data, MAX_LENGTH, "1:%d", client);
-    rc = sendto(client_sockfd, data, strlen(data), 0,
-        (const struct sockaddr *)&client_addr, sizeof(struct sockaddr_un));
-    if (rc < 0) {
-        ALOGE("%s: failed to send: %s", __func__, strerror(errno));
-    }
-}
-
 static void power_set_interactive(__attribute__((unused)) struct power_module *module, int on)
 {
     if (last_state == -1) {
@@ -332,7 +310,6 @@ static void power_set_interactive(__attribute__((unused)) struct power_module *m
     ALOGV("%s %s", __func__, (on ? "ON" : "OFF"));
     if (on) {
         sync_thread(0);
-        touch_boost();
     } else {
         sync_thread(1);
     }
@@ -346,7 +323,6 @@ static void power_hint( __attribute__((unused)) struct power_module *module,
     switch (hint) {
         case POWER_HINT_INTERACTION:
             ALOGV("POWER_HINT_INTERACTION");
-            touch_boost();
             break;
 #if 0
         case POWER_HINT_VSYNC:
