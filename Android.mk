ba726a00457c4d4495cf6f73a4f490babf09f8fb
Marco Marinho <marcomarinho12@gmail.com>
2017-06-16 01:49:38 +0530

    hammerhead: Restructure & cleanup device tree
    * Based on: https://github.com/LineageOS/android_device_lge_hammerheadcaf/commit/f87c73520917ca0a83ea019309c56fc8cccb23d5 by @myfluxi
    * Removed useless folders & files
    * Cleaned up the configs
    * Adapted audio to caf
    * Adapted media codecs to caf
    * Changed some kernel configs to match with caf
    * Build msm8974 copybit
    * Enabled hardware AAC encoder
    * Enabled triple framebuffers
    * Deleted egl.cfg (it's in system now)
    
    Change-Id: Ifcd9fc75e79ff46605031706e2e8b0337a548fe9
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,6 @@
 #
-# Copyright 2013 The Android Open Source Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 Paranoid Android
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,18 +15,9 @@
 # limitations under the License.
 #
 
-# This contains the module build definitions for the hardware-specific
-# components for this device.
-#
-# As much as possible, those components should be built unconditionally,
-# with device-specific names to avoid collisions, to avoid device-specific
-# bitrot and build breakages. Building a component unconditionally does
-# *not* include it on all devices, so it is safe even with hardware-specific
-# components.
-
-ifneq ($(filter hammerhead, $(TARGET_DEVICE)),)
-
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),hammerhead)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
