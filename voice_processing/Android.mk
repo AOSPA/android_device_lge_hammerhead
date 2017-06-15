944c3c3631b1826b8d75f42cadf5fb8ca9886a8f
Marco Marinho <marcomarinho12@gmail.com>
2017-06-16 01:49:38 +0530

    hammerhead: Restructure & cleanup device tree
    * Based on: https://github.com/LineageOS/android_device_lge_hammerheadcaf/commit/f87c73520917ca0a83ea019309c56fc8cccb23d5 by @myfluxi
    * Removed useless folders & files
    * Cleaned up the configs
    * Adapted audio to caf
    * Adapted media codecs to caf
    * Changed some kernel configs to match with caf
    * Build msm8974 copybit
    * Enabled hardware AAC encoder
    * Enabled triple framebuffers
    
    Change-Id: Ifcd9fc75e79ff46605031706e2e8b0337a548fe9
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>

--- voice_processing/Android.mk
+++ voice_processing/Android.mk
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifneq ($(filter hammerhead, $(TARGET_DEVICE)),)
-
 LOCAL_PATH:= $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),hammerhead)
 
 include $(CLEAR_VARS)
 
