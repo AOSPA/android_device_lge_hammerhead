5113629b368fd3164db406c04db0ba31a7377804
Marco Marinho <marcomarinho12@gmail.com>
2017-07-08 08:51:16 +0530

    hammerhead: change kernel config [we're paranoid]
    * since we now use hammerhead_defconfig in the kernel we need this change too.
    * generalize the defconfig name back to the stock name
    
    Change-Id: Ie392e3006f04dbdbab45772e7b157e1c7ec52f05
    Signed-off-by: Marco Marinho <marcomarinho12@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,7 +41,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=hammerhead user_debug=31 maxcpus=2 msm_watchdog_v2.enable=1
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02900000 --tags_offset 0x02700000
 TARGET_KERNEL_ARCH := arm
-TARGET_KERNEL_CONFIG := lineageos_hammerhead_defconfig
+TARGET_KERNEL_CONFIG := hammerhead_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
 TARGET_KERNEL_APPEND_DTB := true
 
